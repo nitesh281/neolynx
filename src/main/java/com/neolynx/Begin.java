83de1893218d814cdc5ccaaf48b690c6f9f29ea2
Nitesh Garg <niteshgarg@gmail.com>
2014-10-24 11:53:38 +0530

    First commit

---
+++ src/main/java/com/neolynx/Begin.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.neolynx;
+
+/**
+ * @author nitesh.garg
+ *
+ */
+public class Begin {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Let the interesting times begin!!!");
+	}
+
+}
