490c64c14179a9e3db9a710a1df12dcc3e858cbc
Nitesh Garg <niteshgarg@gmail.com>
2014-10-25 00:28:13 +0530

    Spring is in

---
+++ src/main/java/com/neolynx/pojo/SpringTestMessage.java
@@ -0,0 +1,15 @@
+package com.neolynx.pojo;
+
+public class SpringTestMessage {
+
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String sayHello() {
+		return "Hello Then... Spring-3 is in the house. Say " + message;
+	}
+
+}
