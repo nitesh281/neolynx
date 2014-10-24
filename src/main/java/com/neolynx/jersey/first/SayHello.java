1e15ebed198555f4ee7894a3660cb6f8e4166ef2
Nitesh Garg <niteshgarg@gmail.com>
2014-10-24 16:37:35 +0530

    Got Jersey Working

--- src/main/java/com/neolynx/jersey/first/SayHello.java
+++ src/main/java/com/neolynx/jersey/first/SayHello.java
@@ -5,15 +5,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-//Plain old Java Object it does not extend as class or implements 
-//an interface
-
-//The class registers its methods for the HTTP GET request using the @GET annotation. 
-//Using the @Produces annotation, it defines that it can deliver several MIME types,
-//text, XML and HTML. 
-
-//The browser requests per default the HTML MIME type.
-
 //Sets the path to base URL + /hello
 @Path("/hello")
 public class SayHello {
@@ -22,14 +13,14 @@ public class SayHello {
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayPlainTextHello() {
-		return "Hello Jersey";
+		return "Let the interesting times begin!!!";
 	}
 
 	// This method is called if XML is request
 	@GET
 	@Produces(MediaType.TEXT_XML)
 	public String sayXMLHello() {
-		return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+		return "<?xml version=\"1.0\"?>" + "<hello> Let the interesting times begin!!!" + "</hello>";
 	}
 
 	// This method is called if HTML is request
@@ -37,7 +28,7 @@ public class SayHello {
 	@Produces(MediaType.TEXT_HTML)
 	public String sayHtmlHello() {
 		return "<html> " + "<title>" + "Hello Jersey" + "</title>"
-				+ "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+				+ "<body><h1>" + "Let the interesting times begin!!!" + "</body></h1>" + "</html> ";
 	}
 
 }
